fc732b2f99d02ac9a6fdb3d775827b599eab4da4
Irfan Totti <irfantotti77777@gmail.com>
2026-02-01 12:18:11 +0530

    Initial Commit

---
+++ Restaurant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restaurant
+//
+//  Created by Irfan on 01/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Irfan on 01/02/26.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
